eb7b9b0845e9cffcb4e9b0c52c18aafe5879cd6d
krishnakrupaj <64538627+krishnakrupaj@users.noreply.github.com>
2020-11-24 19:49:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,14 +12,14 @@ var cars, car1, car2, car3, car4;
 var car1_img,car2_img,car3_img,car4_img,track,gI
 
 function preload(){
-  car1_img = loadImage("images/car1.png");
-  car2_img = loadImage("images/car2.png");
-  car3_img = loadImage("images/car3.png");
-  car4_img = loadImage("images/car4.png");
+  car1_img = loadImage("car1.png");
+  car2_img = loadImage("car2.png");
+  car3_img = loadImage("car3.png");
+  car4_img = loadImage("car4.png");
 
-  track = loadImage("images/track.jpg");
+  track = loadImage("track.jpg");
 
-  gI = loadImage("images/ground.png");
+  gI = loadImage("ground.png");
 }
 
 
